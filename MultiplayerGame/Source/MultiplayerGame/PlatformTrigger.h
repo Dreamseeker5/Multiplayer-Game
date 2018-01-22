c5485f758b03314de122c9e8a42d8c0ea960e0d8
Edgar Hernández <riseofagamer@gmail.com>
2018-01-22 08:52:04 -0400

    S01 L14
    *The trigger volume has overlap events registered.

--- MultiplayerGame/Source/MultiplayerGame/PlatformTrigger.h
+++ MultiplayerGame/Source/MultiplayerGame/PlatformTrigger.h
@@ -15,11 +15,9 @@ public:
 	// Sets default values for this actor's properties
 	APlatformTrigger();
 
-protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
@@ -27,4 +25,9 @@ private:
 	UPROPERTY(VisibleAnywhere)
 		class UBoxComponent* TriggerVolume;
 	
+	UFUNCTION()
+		void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
